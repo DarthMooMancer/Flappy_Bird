f58907eb93529d809dfc39ed2256456c3cc81f2c
DarthMooMancer <dmm.dev@pm.me>
2025-09-04 19:25:16 -0400

    fix: fixed collision of pipe where it only did the first and not all pipes

--- src/main.cpp
+++ src/main.cpp
@@ -40,11 +40,13 @@ int main() {
 		if(pipes.front().m_nodes->m_col < 3) {
 			_delete_pipe(pipes);
 		}
-		for(int i = 0; i < pipes.front().m_size; i++) {
-			if(pipes.front().m_nodes[i].m_col == bird.m_body.m_col) {
-				if(pipes.front().m_nodes[i].m_row == bird.m_body.m_row) {
-					running = false;
-					std::cout << "You died!" << std::endl;
+		for(int j = 0; j < pipes.size(); j++) {
+			for(int i = 0; i < pipes.front().m_size; i++) {
+				if(pipes.at(j).m_nodes[i].m_col == bird.m_body.m_col) {
+					if(pipes.at(j).m_nodes[i].m_row == bird.m_body.m_row) {
+						running = false;
+						std::cout << "You died!" << std::endl;
+					}
 				}
 			}
 		}
